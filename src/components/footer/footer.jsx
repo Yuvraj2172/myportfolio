3e93dd3237fef44a90872ff97bfd7883fcf1687a
Yuvraj Soni <demonsroar2172@gmail.com>
2023-08-05 22:11:47 +0530

    done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {AiOutlineFacebook,AiOutlineInstagram,AiOutlineTwitter} from 'react-icons/ai';
 import './footer.css'
 const footer = () => {
   return (
-   <section id="footer">footer</section>
+    <footer>
+      <a href="#" className='footer_logo' >Yuvraj</a>
+      <ul className='permalinks' >
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://twitter.com"> <AiOutlineTwitter/> </a>
+        <a href="https://instagram.com"> <AiOutlineInstagram/> </a>
+        <a href="https://facebook.com"><AiOutlineFacebook/></a>
+      </div>
+      <div className="footer_copyright">
+        <small>
+          &copy; YUVRAJ SONI
+        </small>
+      </div>
+    </footer>
   )
 }
 
